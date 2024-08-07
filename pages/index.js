5e92932895d6822aa0d69b5a670f7bd83540cd59
Leonardo Jaques <103287079+LeoJaques@users.noreply.github.com>
2024-08-07 01:14:41 +0000

    dia 4: script dev + public visibility

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <>
+        <h1>DESCUBRA QUE JOGADOR DO REAL MADRID VOCÊ É</h1>
+        <p>Preencha o formulário para descobrir que jogado do Real Madrid você seria!</p>
+        </>
+    )
+}
